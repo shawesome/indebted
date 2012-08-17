c80c1b95e56c8de14fc02db0187b05ed17c3b4e7
Martin Shaw <martin.shaw@meanbee.com>
2012-08-17 17:09:51 +0100

    Apply debts based on form data

--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
 $(function() {
+    /* Sync up the checked values and the images */
+    $('input[type=radio]:checked, input[type=checkbox]:checked').each(function() {
+        $(this).next('img').addClass("selected");
+    });
+
+    /* Click handlers */
     $('.buyer img').click(function() {
         $('.buyer img').removeClass("selected");
         $(this).toggleClass("selected");
         $(this).prev("input").click();
-    })
+    });
 
     $('.buyees img').click(function() {
         $(this).toggleClass("selected");
         $(this).prev("input").click();
-    })
+    });
 });
--- meanbee-indebted.py
+++ meanbee-indebted.py
@@ -8,10 +8,16 @@ from google.appengine.ext import db
 
 class Indebted(db.Model):
   """Models a poor individual who finds themselves in debt."""
-  name = db.StringProperty();
-  image = db.StringProperty();
-  email = db.EmailProperty();
-  debt = db.FloatProperty();
+  name = db.StringProperty()
+  image = db.StringProperty()
+  email = db.EmailProperty()
+  debt = db.FloatProperty()
+
+  def decrease_debt(self, amount):
+      self.debt -= amount
+
+  def increase_debt(self, amount):
+      self.debt += amount
 
 class MainPage(webapp2.RequestHandler):
   def get(self):
@@ -42,13 +48,32 @@ class CreateUser(webapp2.RequestHandler):
             self.response.out.write('{status: "FAIL", reason: "Name, email or image not provided. %s, %s, %s"}' % (name, email, image))
 
 
+class AddTransaction(webapp2.RequestHandler):
+    def post(self):
+        buyer_id   = int(self.request.get("buyer"))
+        amount     = float(self.request.get("amount"))
+        reference  = self.request.get("reference")
+        buyee_ids  = map(lambda x:int(x), self.request.get("buyees", allow_multiple=True))
 
+        debt_modification = amount / len(buyee_ids)
 
+        buyer = Indebted.get_by_id(buyer_id)
+        buyer.decrease_debt(debt_modification)
+        buyer.put()
 
+        for buyee_id in buyee_ids:
+            if buyee_id == buyer_id:
+                # Ignore charging the buyer
+                continue
+            buyee = Indebted.get_by_id(buyee_id)
+            buyee.increase_debt(debt_modification)
+            buyee.put()
 
+        self.redirect('/')
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/add_transaction', AddTransaction),
     ('/create_user', CreateUser),
 ], debug=True)
 
