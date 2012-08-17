7e5f983552bdc70d0c27a361837400d65f46e8b5
Martin Shaw <martin.shaw@meanbee.com>
2012-08-17 15:51:30 +0100

    Added frontend for the add transaction form

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 $(function() {
+    $('.buyer img').click(function() {
+        $('.buyer img').removeClass("selected");
+        $(this).toggleClass("selected");
+        $(this).prev("input").click();
+    })
 
+    $('.buyees img').click(function() {
+        $(this).toggleClass("selected");
+        $(this).prev("input").click();
+    })
 });
