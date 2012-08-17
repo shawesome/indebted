623f315a1413bbcadc0427313a78570b6d12fafb
Martin Shaw <martin.shaw@meanbee.com>
2012-08-17 15:21:19 +0100

    A cheeky script which makes users for us

---
+++ initUsers.sh
@@ -0,0 +1,4 @@
+curl -d "name=Martin&email=martin.shaw@meanbee.com&image=http://www.gravatar.com/avatar/229da2427042349c647e3a56183bd97f.png" http://localhost:8080/create_user
+curl -d "name=Nick&email=nick.jones@meanbee.com&image=http://www.gravatar.com/avatar/222f62f43bdcbbae2d2ba55a0d44a038.png" http://localhost:8080/create_user
+curl -d "name=Tom&email=tom.robertshaw@meanbee.com&image=http://www.gravatar.com/avatar/2da528b52a6b7a49d0685b58c654af1f.png" http://localhost:8080/create_user
+curl -d "name=Darren&email=darren.belding@meanbee.com&image=http://www.gravatar.com/avatar/a3c964caf8fc69337f287b3708c97290.png" http://localhost:8080/create_user
