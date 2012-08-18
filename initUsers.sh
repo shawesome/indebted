6a3c48621c37c6a78b972f7736cbef74d0203beb
Martin Shaw <martin.shaw@meanbee.com>
2012-08-18 18:11:56 +0100

    Cheeky script to add users to local install or live install

--- initUsers.sh
+++ initUsers.sh
@@ -1,4 +1,13 @@
+# Update locally
 curl -d "name=Martin&email=martin.shaw@meanbee.com&image=http://www.gravatar.com/avatar/229da2427042349c647e3a56183bd97f.png" http://localhost:8080/create_user
 curl -d "name=Nick&email=nick.jones@meanbee.com&image=http://www.gravatar.com/avatar/222f62f43bdcbbae2d2ba55a0d44a038.png" http://localhost:8080/create_user
 curl -d "name=Tom&email=tom.robertshaw@meanbee.com&image=http://www.gravatar.com/avatar/2da528b52a6b7a49d0685b58c654af1f.png" http://localhost:8080/create_user
 curl -d "name=Darren&email=darren.belding@meanbee.com&image=http://www.gravatar.com/avatar/a3c964caf8fc69337f287b3708c97290.png" http://localhost:8080/create_user
+curl -d "name=Red&email=reddavis@gmail.com&image=http://www.gravatar.com/avatar/8ff2e3d23012979b285f97c33c8f43ba.jpg" http://localhost:8080/create_user
+
+# Update appspot
+#curl -d "name=Martin&email=martin.shaw@meanbee.com&image=http://www.gravatar.com/avatar/229da2427042349c647e3a56183bd97f.png" http://meanbee-indebted.appspot.com/create_user
+#curl -d "name=Nick&email=nick.jones@meanbee.com&image=http://www.gravatar.com/avatar/222f62f43bdcbbae2d2ba55a0d44a038.png" http://meanbee-indebted.appspot.com/create_user
+#curl -d "name=Tom&email=tom.robertshaw@meanbee.com&image=http://www.gravatar.com/avatar/2da528b52a6b7a49d0685b58c654af1f.png" http://meanbee-indebted.appspot.com/create_user
+#curl -d "name=Darren&email=darren.belding@meanbee.com&image=http://www.gravatar.com/avatar/a3c964caf8fc69337f287b3708c97290.png" http://meanbee-indebted.appspot.com/create_user
+#curl -d "name=Red&email=reddavis@gmail.com&image=http://www.gravatar.com/avatar/8ff2e3d23012979b285f97c33c8f43ba.jpg" http://meanbee-indebted.appspot.com/create_user
